c83b1c6b62fefb88867cd4943409282f8eeaa4f4
DsDominator <150493907+DsDominator@users.noreply.github.com>
2024-07-22 08:18:02 +0700

    Add files via upload

---
+++ AccountModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace CrudOperationInMVC.Models
+{
+    public class AccountModel
+    {
+        private CompanyDBDataContext context = null;
+        public AccountModel()
+        {
+            context = new CompanyDBDataContext("Data Source=LAPTOP-CG78U18T\\PHONG;Initial Catalog=ProjectMVC;Integrated Security=True;TrustServerCertificate=True");
+        }
+        
+        public bool Login(string Username, string Password)
+        {
+            bool? res = false;
+            context.sp_Account_Login_Check(Username, Password, ref res);
+            return (res ?? false);
+        }
+
+    }
+
+   
+}
